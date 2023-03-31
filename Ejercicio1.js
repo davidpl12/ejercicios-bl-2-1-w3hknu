886d9285c0b45c07d933b45de73f50ceca83a1e2
davidpl12 <davidpl1202@gmail.com>
2023-03-31 12:54:08 +0200

    Initial commit

---
+++ Ejercicio1.js
@@ -0,0 +1,114 @@
+'Use strict';
+
+let programadores = [
+  {
+    nombre: 'Juan',
+    id: 1,
+    tareas: [
+      {
+        nombreTarea: 'Tarea 1.1',
+        idTarea: 1.1,
+        tiempoDesarrollo: '25 horas',
+      },
+      {
+        nombreTarea: 'Tarea 1.2',
+        idTarea: 1.2,
+        tiempoDesarrollo: '10 horas',
+      },
+      { nombreTarea: 'Tarea 1.3', idTarea: 1.3, tiempoDesarrollo: '2 horas' },
+      {
+        nombreTarea: 'Tarea 1.4',
+        idTarea: 1.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'Lucia',
+    id: 2,
+    tareas: [
+      { nombreTarea: 'Tarea 2.1', idTarea: 2.1, tiempoDesarrollo: '5 horas' },
+      { nombreTarea: 'Tarea 2.2', idTarea: 2.2, tiempoDesarrollo: '1 hora' },
+      {
+        nombreTarea: 'Tarea 2.3',
+        idTarea: 2.3,
+        tiempoDesarrollo: '26 horas',
+      },
+      {
+        nombreTarea: 'Tarea 2.4',
+        idTarea: 2.4,
+        tiempoDesarrollo: '3.75 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'David',
+    id: 3,
+    tareas: [
+      { nombreTarea: 'Tarea 3.1', idTarea: 3.1, tiempoDesarrollo: '7 horas' },
+      {
+        nombreTarea: 'Tarea 3.2',
+        idTarea: 3.2,
+        tiempoDesarrollo: '6 horas',
+      },
+      {
+        nombreTarea: 'Tarea 3.3',
+        idTarea: 3.3,
+        tiempoDesarrollo: '9 horas',
+      },
+      {
+        nombreTarea: 'Tarea 3.4',
+        idTarea: 3.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'Manuel',
+    id: 4,
+    tareas: [
+      { nombreTarea: 'Tarea 4.1', idTarea: 4.1, tiempoDesarrollo: '7 horas' },
+      {
+        nombreTarea: 'Tarea 4.2',
+        idTarea: 4.2,
+        tiempoDesarrollo: '16 horas',
+      },
+      {
+        nombreTarea: 'Tarea 4.3',
+        idTarea: 4.3,
+        tiempoDesarrollo: '29 horas',
+      },
+      {
+        nombreTarea: 'Tarea 4.4',
+        idTarea: 4.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+];
+
+const naturalOrder = (a, b) => a - b;
+
+var array = [];
+for (let i = 0; i < programadores.length; i++) {
+  for (let j = 0; j < programadores[i].tareas.length; j++) {
+    let cadena = programadores[i].tareas[j].tiempoDesarrollo;
+    let indice = cadena.indexOf(' ');
+    array.push(cadena.substring(0, indice));
+  }
+}
+let arrayOrder = array.sort(naturalOrder).reverse();
+//console.log(arrayOrder);
+
+for (let i = 0; i < programadores.length; i++) {
+  for (let j = 0; j < programadores[i].tareas.length; j++) {
+    let cadena = programadores[i].tareas[j].tiempoDesarrollo;
+    let indice = cadena.indexOf(' ');
+    if (cadena.substring(0, indice) == arrayOrder[0]) {
+      console.log(
+        'EJERCICIO 1: La que mas tiempo necesita es la ' +
+          programadores[i].tareas[j].nombreTarea
+      );
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ejercicios-bl-2-1-w3hknu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ejercicios-bl-2-1-w3hknu)
